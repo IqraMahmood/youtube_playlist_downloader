710f5e8e52d2c0e2f6bd00d6c631d212a2d4f113
IqraMahmood <87108359+IqraMahmood@users.noreply.github.com>
2023-02-27 11:13:58 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from pytube import Playlist
-import zipfile
-import os
-import base64
-
-
-# Set up Streamlit page
+from PIL import Image
+# # Set up Streamlit page
+image = Image.open('app.png')
 st.title("YouTube Playlist Downloader")
 playlist_url = st.text_input("Enter the URL of the YouTube playlist you want to download:")
 download_button = st.button("Download Playlist")
@@ -16,23 +13,13 @@ if download_button:
         # Create a Playlist object from the given URL
         playlist = Playlist(playlist_url)
 
-        # Create a directory to store the downloaded videos
-        dir_path = os.path.join(os.getcwd(), "videos")
-        os.makedirs(dir_path, exist_ok=True)
+        # Create a download link for each video in the playlist
+        for video in playlist.videos:
+            st.write(f"Generating download link for {video.title}...")
+            video_url = video.streams.get_highest_resolution().url
+            st.markdown(f'<a href="{video_url}" download="{video.title}.mp4">Download {video.title}</a>', unsafe_allow_html=True)
+            st.write(f"Download link for {video.title} generated!")
 
-        # Download each video in the playlist to the videos directory
-        for i, video in enumerate(playlist.videos):
-            st.write(f"Downloading {video.title}...")
-            video.streams.get_highest_resolution().download(output_path=dir_path)
-            st.write(f"{i+1}/{len(playlist)} videos downloaded!")
-
-        # Create a zip file of the downloaded videos
-        zip_file_path = os.path.join(os.getcwd(), "playlist.zip")
-        with zipfile.ZipFile(zip_file_path, "w") as zip_file:
-            for video_file in os.listdir(dir_path):
-                video_file_path = os.path.join(dir_path, video_file)
-                zip_file.write(video_file_path, arcname=video_file)
-        st.success("Playlist download complete!")
-        st.write(f"Download the complete playlist as a zip file: [playlist.zip](data:application/zip;base64,{base64.b64encode(open(zip_file_path, 'rb').read()).decode('utf-8')})")
+        st.success("Playlist download links generated successfully!")
     except Exception as e:
         st.error(f"An error occurred: {e}")
